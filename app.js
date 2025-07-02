e2c33030730d66e2ae82c191c8a17a82877be749
Andrii Sokha <sokha.andrey.vn@gmail.com>
2025-07-03 01:38:30 +0300

    Added CORS for interaction with frontend

--- app.js
+++ app.js
@@ -4,9 +4,16 @@ const rateLimit = require('express-rate-limit');
 const dotenv = require('dotenv');
 const errorHandler = require('./middlewares/errorHandler');
 const authRoutes = require('./routes/authRoutes');
+const cors = require('cors');
 
 const app = express();
 dotenv.config();
+
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
 
 app.use(helmet());
 app.use(express.json({limit: '10mb'}));
--- repositories/userRepository.js
+++ repositories/userRepository.js
@@ -1,6 +1,6 @@
 const fs = require('fs').promises;
 const path = require('path');
-const USERS_FILE = path.join(__dirname, '../../users.json');
+const USERS_FILE = path.join(__dirname, '../users.json');
 
 exports.loadUsers = async () => {
     try {
@@ -18,3 +18,5 @@ exports.loadUsers = async () => {
 exports.saveUsers = async (users) => {
     await fs.writeFile(USERS_FILE, JSON.stringify(users, null, 2));
 };
+
+console.log('ABSOLUTE USERS FILE PATH:', USERS_FILE);
