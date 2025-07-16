caed644894c06096d4828832d16115fac01adba2
Andrii Sokha <sokha.andrey.vn@gmail.com>
2025-07-16 19:58:50 +0300

    Added login interaction with API

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,39 +1,74 @@
-import { Link } from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 import '../styles/loginStyle.css';
 import logo from '../assets/logo.png';
+import {useState} from 'react';
+import {login} from '../services/api';
 
 
 const LoginPage = () => {
+
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
+
+    const [error, setError] = useState('')
+    const [success, setSuccess] = useState('');
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        setFormData({...formData, [e.target.name]: e.target.value});
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setError('');
+        setSuccess('');
+
+        try {
+            const result = await login(formData);
+            setSuccess(result.message);
+            setTimeout(() => navigate('/'), 1500);
+        } catch (err) {
+            setError(err.message);
+        }
+    };
+
+
     return (
         <div className="login-page">
-        <div className="login-container">
-            <div className="login-content">
-                <img src={logo} alt="Logo" className="logo" />
-                <div className="login-box">
-                    <h2>З поверненням</h2>
-                    <form className="login-form">
-                        <input type="email" placeholder="Email" required />
-                        <input type="password" placeholder="Пароль" required />
-                        <div className="forgot-link">
-                            <Link to="/forgot-password">Забули пароль?</Link>
-                        </div>
-                        <button type="submit" className="login-btn">Увійти</button>
-                        <button
-                            type="button"
-                            className="google-btn"
-                            onClick={() => {
-                                window.location.href = 'http://localhost:3000/auth/google';
-                            }}
-                        >
-                            Увійти через Google
-                        </button>
+            <div className="login-container">
+                <div className="login-content">
+                    <img src={logo} alt="Logo" className="logo"/>
+                    <div className="login-box">
+                        <h2>З поверненням</h2>
+                        <form className="login-form" onSubmit={handleSubmit}>
+                            <input name="email" type="email" placeholder="Email" value={formData.email}
+                                   onChange={handleChange} required/>
+                            <input name="password" type="password" placeholder="Пароль" value={formData.password}
+                                   onChange={handleChange} required/>
+                            <div className="forgot-link">
+                                <Link to="/forgot-password">Забули пароль?</Link>
+                            </div>
+                            <button type="submit" className="login-btn">Увійти</button>
+                            <button
+                                type="button"
+                                className="google-btn"
+                                onClick={() => {
+                                    window.location.href = 'http://localhost:3000/auth/google';
+                                }}
+                            >
+                                Увійти через Google
+                            </button>
 
-                        <div className="reg-link">
-                            <Link to="/registration">Немає аккаунту? Зареєструватись</Link>
-                        </div>
-                    </form>
+                            <div className="reg-link">
+                                <Link to="/registration">Немає аккаунту? Зареєструватись</Link>
+                            </div>
+                            {error && <p style={{color: 'red'}}>{error}</p>}
+                            {success && <p style={{color: 'green'}}>{success}</p>}
+                        </form>
+                    </div>
                 </div>
-            </div>
             </div>
         </div>
     );
--- src/services/api.js
+++ src/services/api.js
@@ -17,3 +17,21 @@ export async function register({ username, email, password}) {
 
     return data;
 }
+
+export async function login({ email, password}) {
+    const response = await fetch(`${API_BASE}/login`, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({email, password})
+    });
+
+    const data = await response.json();
+
+    if(!response.ok) {
+        throw new Error(data.errors?.[0] || data.error || 'Помилка авторизації')
+    }
+
+    return data;
+}
